9d37859027f06fee4f256d0c15f84ed1c5c9a751
NaNoMelo <nathan.lamey68@orange.fr>
2022-05-16 17:21:57 +0200

    Parser fonctionnel
    Signed-off-by: NaNoMelo <nathan.lamey68@orange.fr>

--- saisie.c
+++ saisie.c
@@ -34,6 +34,7 @@ int askTaillePlateau() {
 void askDeplacement(int taillePlateau, int joueur, int **move) {
     int state;
     char unparsedMove[10];
+    int number;
     do {
         state = 0;
         move[0][0] = -1;
@@ -47,15 +48,10 @@ void askDeplacement(int taillePlateau, int joueur, int **move) {
         for (int i = 0; i < strlen(unparsedMove); ++i) {
             while (unparsedMove[i] == ' ') { i++; }
             if (state % 2) {
-                if (isdigit(unparsedMove[i]) && unparsedMove[i] - '0' > 0 && unparsedMove[i] - '0' <= taillePlateau) {
-                    if (isdigit(unparsedMove[i + 1]) && unparsedMove[i + 1] - '0' >= 0 &&
-                        unparsedMove[i + 1] - '0' <= taillePlateau) {
-                        move[1][(state - 1) / 2] = (unparsedMove[i - 1] - '0') * 10 + unparsedMove[i] -
-                                                   '0';
-                        i++;
-                    } else {
-                        move[1][(state - 1) / 2] = unparsedMove[i] - '0' - 1;
-                    }
+                number = strtol(&unparsedMove[i], NULL, 10);
+                if (number <= 12 && number >= 1) {
+                    move[1][(state - 1) / 2] = number - 1;
+                    if (isdigit(unparsedMove[i + 1]))i++;
                 } else {
                     printf("Mouvement invalide\n");
                     break;
@@ -70,7 +66,6 @@ void askDeplacement(int taillePlateau, int joueur, int **move) {
             }
             state++;
         }
-
         printf("%d %d\n%d %d\n", move[0][0], move[0][1], move[1][0], move[1][1]);
     } while (!(move[0][0] + 1 && move[0][1] + 1 && move[1][0] + 1 && move[1][1] + 1));
 }
